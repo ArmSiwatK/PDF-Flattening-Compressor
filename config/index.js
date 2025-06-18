f2fc75e69d2a393502015609e09a317735b10197
Siwat Kanjanapanitkun <arm.siwatk@gmail.com>
2025-06-18 17:19:16 +0700

    refactor: Actually use config import

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ module.exports = {
     inputDir: path.resolve(__dirname, '..', 'input'),
     outputDir: path.resolve(__dirname, '..', 'output'),
     convertOptions: {
-        format: 'jpeg',
-        jpeg_quality: 75,
+        format: 'jpg',
+        jpeg_quality: 100,
         out_prefix: 'page',
         poppler_path: path.resolve(__dirname, '..', 'poppler-24.08.0', 'Library', 'bin'),
         scale: 2000,
--- src/main.js
+++ src/main.js
@@ -7,24 +7,23 @@ const config = require('../config');
 
 const main = async () => {
     try {
+        await fs.rm(config.outputDir, { recursive: true, force: true });
+        await fs.mkdir(config.outputDir, { recursive: true });
+
         const files = await fs.readdir(config.inputDir);
         const pdfFiles = files.filter(file => path.extname(file).toLowerCase() === '.pdf');
-
         if (!pdfFiles.length) throw new Error('No PDF files found in input directory');
 
         await Promise.all(pdfFiles.map(async pdfFile => {
             const inputPath = path.join(config.inputDir, pdfFile);
             const outputPath = path.join(config.outputDir, pdfFile);
+            const outputDir = path.join(config.outputDir, path.basename(pdfFile, '.pdf'))
             await processSinglePDF(inputPath, outputPath, {
-                ...config,
-                convertOptions: {
-                    ...config.convertOptions,
-                    out_dir: path.join(config.outputDir, path.basename(pdfFile, '.pdf'))
-                }
+                ...config, convertOptions: { ...config.convertOptions, out_dir: outputDir }
             });
         }));
-
         console.log('[+] All PDFs processed successfully.');
+
     } catch (error) {
         console.error('Error:', error);
     }
