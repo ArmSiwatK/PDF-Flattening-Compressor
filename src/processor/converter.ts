c4891502c3e6598014384b9723356785b1aa7e3e
Siwat Kanjanapanitkun <arm.siwatk@gmail.com>
2026-05-18 11:38:42 +0700

    refactor: Separate functions for converter

--- src/processor/converter.ts
+++ src/processor/converter.ts
@@ -1,4 +1,5 @@
-import fs from 'fs/promises';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { PDFDocument as LibPDFDocument } from 'pdf-lib';
 import { Poppler } from 'node-poppler';
 import ora from 'ora';
@@ -13,23 +14,43 @@ export interface ConvertPDFToImagesParams {
 }
 
 
-export const convertPDFToImages = async ({ inputPDFPath, tempDir, convertOptions, }: ConvertPDFToImagesParams): Promise<string[]> => {
-    const pdfDoc = await LibPDFDocument.load(await fs.readFile(inputPDFPath));
-    const pageCount = pdfDoc.getPageCount();
+const getPDFPageCount = async (inputPDFPath: string): Promise<number> => {
+    const pdfBytes = await fs.readFile(inputPDFPath);
+    const pdfDoc = await LibPDFDocument.load(pdfBytes);
+    return pdfDoc.getPageCount();
+};
 
-    const spinner = ora('Converting PDF to high-quality images...').start();
-    const start = Date.now();
 
-    await poppler.pdfToCairo(
-        inputPDFPath,
-        `${tempDir}/${convertOptions.outPrefix}`,
-        { jpegFile: true, firstPageToConvert: 1, lastPageToConvert: pageCount, scalePageTo: convertOptions.scalePageTo }
-    );
-    spinner.succeed(`Conversion complete in ${((Date.now() - start) / 1000).toFixed(2)}s`);
+const convertWithPoppler = async (inputPDFPath: string, tempDir: string, pageCount: number, convertOptions: ConvertOptions): Promise<void> => {
+    const options = {
+        jpegFile: true,
+        firstPageToConvert: 1,
+        lastPageToConvert: pageCount,
+        scalePageTo: convertOptions.scalePageTo
+    }
+    await poppler.pdfToCairo(inputPDFPath, path.join(tempDir, convertOptions.outPrefix), options);
+};
 
+
+const getGeneratedImages = async (tempDir: string): Promise<string[]> => {
     const images = (await fs.readdir(tempDir))
         .filter(file => file.endsWith('.jpg'))
         .sort();
     if (!images.length) throw new Error('No images generated from PDF');
     return images;
+};
+
+
+export const convertPDFToImages = async ({ inputPDFPath, tempDir, convertOptions }: ConvertPDFToImagesParams): Promise<string[]> => {
+    const pageCount = await getPDFPageCount(inputPDFPath);
+    const spinner = ora('Converting PDF to high-quality images...').start();
+    const start = Date.now();
+    try {
+        await convertWithPoppler(inputPDFPath, tempDir, pageCount, convertOptions);
+        spinner.succeed(`Conversion complete in ${((Date.now() - start) / 1000).toFixed(2)}s`);
+        return await getGeneratedImages(tempDir);
+    } catch (error) {
+        spinner.fail('PDF conversion failed.');
+        throw error;
+    }
 };
